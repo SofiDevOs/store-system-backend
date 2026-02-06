6c43a366156b8f26a38238db2a4d095a00e4cbbd
LuisLunaV <luis.a.luna.v@outlook.com>
2026-02-05 23:02:07 -0600

    Modificacion en variables de entorno importadas de .env

--- src/config/envs.config.ts
+++ src/config/envs.config.ts
@@ -3,22 +3,12 @@ dotenv.config({ path: ".env" });
 
 const {
   PORT,
-  DB_PORT,
-  DB_HOST,
-  DB_DATABASE,
-  DB_PASSWORD,
-  DB_USER,
   JWT_SECRET_KEY,
   DATABASE_URL
 } = process.env;
 
 export {
   PORT,
-  DB_PORT,
-  DB_HOST,
-  DB_DATABASE,
-  DB_PASSWORD,
-  DB_USER,
   JWT_SECRET_KEY,
   DATABASE_URL
 };
