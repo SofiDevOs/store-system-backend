e1e16d0dedeb54ce2365e3cbd6f69cfeac014424
SofiDev <angiemeow89@gmail.com>
2026-02-19 23:23:24 -0600

    🐛 Fix: connect password hashing extension to application Prisma client

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,10 +1,1 @@
-import { PrismaClient } from "@prisma/client";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
-import { DATABASE_URL } from "./envs.config";
-
-// Remove 'file:' prefix if present
-const dbPath = (DATABASE_URL || "./data/dev.db").replace("file:", "");
-const adapter = new PrismaBetterSqlite3({ url: dbPath });
-const prisma = new PrismaClient({ adapter });
-
-export { prisma };
+export { prisma } from "../../prisma/src/index";
